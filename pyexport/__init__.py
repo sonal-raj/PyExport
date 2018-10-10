2b2cca4313ef8596ec6e53cc0aaa28b8e3648c14
sonal-raj <sonal.nitjsr@gmail.com>
2018-10-11 00:44:46 +0530

    Initiate the package

---
+++ pyexport/__init__.py
@@ -0,0 +1,4 @@
+__version__ = (0, 0, 1)
+__author__ = 'Sonal Rajd'
+__author_email__ = 'sonal.nitjsr@gmail.com'
+__description__ = 'Library to export stuff into beautiful, supported and compatible formats''
