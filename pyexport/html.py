b5d3eed69b51a5c045e6e513c1d7428090ef4127
sonal-raj <sonal.nitjsr@gmail.com>
2018-10-12 00:29:09 +0530

    Initiate html exporter

---
+++ pyexport/html.py
@@ -0,0 +1,15 @@
+"""
+Create a html document and it's components programatically 
+with python.
+
+Usage:
+>> d = Document("This is an Html Page")
+
+"""
+
+class Document:
+    def __init__(self, header):
+        return 
+
+if __name__=="__main__":
+    exit
